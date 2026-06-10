265d7c6501123812e3a90e0859f8c7bb6570140c
Elias_Poseidon <elias.schwarzmann09@htl-salzburg.ac.at>
2026-06-10 14:15:31 +0200

    calculate first guess

---
+++ Assets/MyGame/Scripts/Calculate.cs
@@ -0,0 +1,24 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Calculate : MonoBehaviour
+{
+    public int guesMin;
+    public int guesMax;
+    public int guess;
+    [SerializeField]
+    private Text Guestxt;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        guess = (guesMin + guesMax) / 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Guestxt.text = "is your Number " + guess + "?";
+        
+    }
+}
