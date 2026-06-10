63a231fc0fbdd6a35cc3be83f217f09705e38249
Elias_Poseidon <elias.schwarzmann09@htl-salzburg.ac.at>
2026-06-10 15:01:51 +0200

    add correct answer state close #6

--- Assets/MyGame/Scripts/Calculate.cs
+++ Assets/MyGame/Scripts/Calculate.cs
@@ -7,28 +7,43 @@ public class Calculate : MonoBehaviour
     public int guesMin;
     public int guesMax;
     public int guess;
+    public GameObject Button1;
+    public GameObject Button2;
     [SerializeField]
     private Text Guestxt;
+    private bool cangues = true ;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         guess = (guesMin + guesMax) / 2;
+        Guestxt.text = "is your Number " + guess + "?";
     }
 
     // Update is called once per frame
     void Update()
     {
-        Guestxt.text = "is your Number " + guess + "?";
+        if (cangues == true)
+        {
+            Guestxt.text = "is your Number " + guess + "?";
+        }
         
     }
     public void guesHigher()
     {
-        guesMin = guess -1;
+        guesMin = guess +1;
         guess = (guesMin + guesMax) / 2;
     }
     public void guessLower()
     {
         guesMax = guess - 1;
         guess = (guesMin + guesMax) / 2;
+    }
+    public void CorrectGues()
+    {
+        cangues = false;
+        Guestxt.fontSize = 30;
+        Guestxt.text = "I guesed your Number " + guesMin + "!";
+        Button1.SetActive(false);
+        Button2.SetActive(false);
     }
 }
